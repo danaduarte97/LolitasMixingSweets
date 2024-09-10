beed2cc9418fa83640a8cfd03adee52019f580d7
danaduarte97 <danaduarte97@gmail.com>
2024-09-10 20:04:00 -0300

    nuevo main

--- js/script.js
+++ js/script.js
@@ -17,9 +17,6 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-
-
-
     /*Modal--------------*/
     var exampleModal = document.getElementById('exampleModal');
 
@@ -38,4 +35,20 @@ document.addEventListener('DOMContentLoaded', function () {
             console.error('Modal title or image element not found');
         }
     });
+
+    /*Swiper----------
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });*/
+
 });
